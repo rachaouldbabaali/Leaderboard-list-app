84c9db9a5c152af70a058c2c67b9a914efb1f443
racha ouldbabaali <rachaould@gmail.com>
2023-05-16 02:29:06 +0100

    add first files

--- dist/bundle.js
+++ dist/bundle.js
@@ -3,6 +3,36 @@ var __webpack_exports__ = {};
 /*!**********************!*\
   !*** ./src/index.js ***!
   \**********************/
-
+var leaderboardList = document.getElementById('leaderboard-list');
+var addForm = document.getElementById('add-form');
+// eslint-disable-next-line prefer-const
+var scores = [];
+function addScoreToList(name, score) {
+  scores.push({
+    name: name,
+    score: score
+  });
+  scores.sort(function (a, b) {
+    return b.score - a.score;
+  });
+  leaderboardList.innerHTML = '';
+  scores.forEach(function (score, index) {
+    var listItem = document.createElement('li');
+    var listItemText = document.createTextNode("".concat(index + 1, ". ").concat(score.name, ": "));
+    var listItemScore = document.createElement('span');
+    listItemScore.textContent = score.score;
+    listItem.appendChild(listItemText);
+    listItem.appendChild(listItemScore);
+    leaderboardList.appendChild(listItem);
+  });
+}
+addForm.addEventListener('submit', function (event) {
+  event.preventDefault();
+  var name = document.getElementById('name').value;
+  var score = document.getElementById('score').value;
+  addScoreToList(name, score);
+  addForm.reset();
+});
 /******/ })()
 ;
+//# sourceMappingURL=bundle.js.map
---
+++ src/index.js
@@ -0,0 +1,30 @@
+const leaderboardList = document.getElementById('leaderboard-list');
+const addForm = document.getElementById('add-form');
+// eslint-disable-next-line prefer-const
+let scores = [];
+
+function addScoreToList(name, score) {
+  scores.push({ name, score });
+  scores.sort((a, b) => b.score - a.score);
+
+  leaderboardList.innerHTML = '';
+
+  scores.forEach((score, index) => {
+    const listItem = document.createElement('li');
+    const listItemText = document.createTextNode(`${index + 1}. ${score.name}: `);
+    const listItemScore = document.createElement('span');
+    listItemScore.textContent = score.score;
+
+    listItem.appendChild(listItemText);
+    listItem.appendChild(listItemScore);
+    leaderboardList.appendChild(listItem);
+  });
+}
+
+addForm.addEventListener('submit', (event) => {
+  event.preventDefault();
+  const name = document.getElementById('name').value;
+  const score = document.getElementById('score').value;
+  addScoreToList(name, score);
+  addForm.reset();
+});
